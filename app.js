e9c36fb17cf6d607a83eba110a1d24917a868dd2
seu-nome-de-usuario <darioflauta@gmail.com>
2026-01-16 00:43:17 -0300

    Fix: ajustando estrutura de pastas e rotas vercel

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
-const config = require('./config/config');
-const iaController = require('./controllers/iaController');
+const config = require('./js-backend/config/config');
+const iaController = require('./js-backend/controllers/iaController');
 
 const app = express();
 app.use(bodyParser.json());
